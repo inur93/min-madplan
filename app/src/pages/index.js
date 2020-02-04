d8e2f6632355a23911d487c2c59dadd3327b038c
Rúni Egholm Vørmadal <runivormadal@gmail.com>
2020-02-04 20:09:58 +0100

    initial commit

---
+++ app/src/pages/index.js
@@ -0,0 +1,43 @@
+import React from "react"
+import { Link, graphql } from "gatsby"
+
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+
+const IndexPage = ({data}) => {
+  
+  return (
+
+  <Layout>
+    <SEO title="Home" />
+    <h1>Hi people</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Now go build something great.</p>
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+      <Image />
+    </div>
+    <ul>
+      {data.allStrapiRecipeItem.edges.map(({node}) => <li key={node.name}>
+        <h2>{node.name}</h2>
+      </li>)}
+    </ul>
+    <Link to="/page-2/">Go to page 2</Link>
+  </Layout>
+)
+      }
+
+export default IndexPage
+
+export const pageQuery = graphql`
+ {
+  allStrapiRecipeItem {
+    distinct(field: name)
+    edges {
+      node {
+        name
+      }
+    }
+  }
+}
+`
