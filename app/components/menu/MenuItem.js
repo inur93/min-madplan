73ade7bcf695e8544cdddf2ec5f3c51df55463e4
Rúni Egholm Vørmadal <runivormadal@gmail.com>
2020-04-09 14:52:19 +0200

    added styling for front page and fixed image urls

--- app/components/menu/MenuItem.js
+++ app/components/menu/MenuItem.js
@@ -1,41 +1,18 @@
 import { Header } from 'semantic-ui-react';
 import Link from 'next/link';
 import { absUrl } from '../../functions/imageFunctions';
+import './menu-item.scss';
 
 
 export default function MenuItem({ image, title, link }) {
     const {url} = image || {};
 
     return <div className="menu-item" style={{backgroundImage: `url('${absUrl(url)}')`}}>
-        <Header className="menu-item-header" as="a">
+        <Header as='h2' className="menu-item-header">
             {title}
         </Header>
         <Link href={link}>
             <a className="menu-item-link"></a>
         </Link>
-        <style jsx>{`
-        .menu-item{
-            flex-grow: 1;
-            position: relative;
-            border: 2px solid white;
-            background-size: cover;
-        }
-        :global(.menu-item-header){
-            position: absolute;
-            top: 50%;
-            left: 50%;
-            transform: translate(-50%, -50%);
-            font-size: 50px;
-            margin: 0;
-            color: white;
-        }
-        .menu-item-link{
-            position: absolute;
-            top: 0;
-            left: 0;
-            width: 100%;
-            height: 100%;
-        }
-        `}</style>
     </div>
 }
--- app/functions/imageFunctions.js
+++ app/functions/imageFunctions.js
@@ -1,5 +1,5 @@
 
 const baseUrl = (process.env.NODE_ENV === 'production'
-    ? 'https://min-madplan.herokuapp.com/'
-    : 'http://localhost:1337/');
+    ? 'https://min-madplan.herokuapp.com'
+    : 'http://localhost:1337');
 export const absUrl = (url) => `${baseUrl}${url}`;
