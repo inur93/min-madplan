030fc6e96909a711bada7fde78f3680e68345c10
Rúni Egholm Vørmadal <runivormadal@gmail.com>
2020-04-11 12:02:15 +0200

    fixed other import issues

--- app/_api/api.js
+++ app/_api/api.js
@@ -1,10 +1,48 @@
 import axios from 'axios';
+import cookie from 'js-cookie';
+import ServerCookie from 'next-cookies';
 
 const getBaseUrl = () => {
     if (process.env.NODE_ENV === 'production') {
         return 'https://min-madplan.herokuapp.com/';
     }
     return process.browser ? 'http://localhost:1337/' : 'http://cms:1337/';
+}
+
+
+function parseJwt(token) {
+    if (!token) return;
+    var base64Url = token.split('.')[1];
+    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    var jsonPayload = decodeURIComponent(atob(base64).split('').map(function (c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+
+    return JSON.parse(jsonPayload);
+};
+const validateCookie = (token) => {
+    try {
+        const parsed = parseJwt(token);
+        return true;
+    } catch (e) {
+        console.error('error in cookie', e);
+        return false;
+    }
+}
+const getCookie = (ctx) => {
+    if (ctx) {
+        return ServerCookie(ctx).jwt;
+    } else {
+        const jwt = cookie.get('jwt');
+        if (validateCookie(jwt)) {
+            return jwt;
+        }
+
+        if (jwt && !ctx) {
+            cookie.remove('jwt');
+        }
+    }
+    return false;
 }
 
 export const getApi = (ctx) => axios.create({
--- app/_api/authApi.js
+++ app/_api/authApi.js
@@ -2,42 +2,6 @@ import cookie from 'js-cookie';
 import ServerCookie from 'next-cookies';
 import { getApi } from './api';
 
-function parseJwt(token) {
-    if (!token) return;
-    var base64Url = token.split('.')[1];
-    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-    var jsonPayload = decodeURIComponent(atob(base64).split('').map(function (c) {
-        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-    }).join(''));
-
-    return JSON.parse(jsonPayload);
-};
-const validateCookie = (token) => {
-    try {
-        const parsed = parseJwt(token);
-        return true;
-    } catch (e) {
-        console.error('error in cookie', e);
-        return false;
-    }
-}
-const getCookie = (ctx) => {
-    if (ctx) {
-        return ServerCookie(ctx).jwt;
-    } else {
-        const jwt = cookie.get('jwt');
-        if (validateCookie(jwt)) {
-            return jwt;
-        }
-
-        if (jwt && !ctx) {
-            cookie.remove('jwt');
-        }
-    }
-    return false;
-}
-
-
 const setUserAndToken = (user, token) => {
     cookie.set("user", user && JSON.stringify(user), { expires: 7 });
     if (token) {
