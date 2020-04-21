b66e0c0921082525370f2073487b685acb19d261
Rúni Egholm Vørmadal <runivormadal@gmail.com>
2020-04-21 23:32:11 +0200

    trying env variables again

--- app/next.config.js
+++ app/next.config.js
@@ -2,25 +2,24 @@ const withSourceMaps = require('@zeit/next-source-maps')
 const withSass = require('@zeit/next-sass');
 const withCss = require('@zeit/next-css');
 
-/**
- *  env: {
+module.exports = {
+  env: {
     //expose base url to code running client side.
     BASE_URL: process.env.BASE_URL
   },
-  ...
- */
-module.exports = withSass(withCss(withSourceMaps({
-  webpack: function (config) {
-    config.module.rules.push({
-      test: /\.(eot|woff|woff2|ttf|svg|png|jpg|gif)$/,
-      use: {
-        loader: 'url-loader',
-        options: {
-          limit: 100000,
-          name: '[name].[ext]'
+  ...withSass(withCss(withSourceMaps({
+    webpack: function (config) {
+      config.module.rules.push({
+        test: /\.(eot|woff|woff2|ttf|svg|png|jpg|gif)$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 100000,
+            name: '[name].[ext]'
+          }
         }
-      }
-    })
-    return config
-  }
-})))
+      })
+      return config
+    }
+  })))
+}
